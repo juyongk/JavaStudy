1c88ce73709558ebdd6f672be31520993c3be3ce
juyongk <kimjuyong1108@gmail.com>
2023-07-17 18:13:48 +0900

    2023-07-17 JAVA수업

---
+++ src/quiz/QuRockPaperScissors.java
@@ -0,0 +1,72 @@
+package quiz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+게임설명 : 가위바위보 게임을 메소드를 통해 구현한다.
+난수생성기를 이용하여 1, 2, 3중 하나의 숫자를 생성한다.
+사용자가 가위(1),바위(2),보(3) 중 하나를 낸다.
+승부를 판단하여 출력한다.
+1,2,3 이외의 숫자를 입력하면 잘못된 입력을 알려주고 재입력을 요구한다.
+숫자입력을 잘못한 경우는 게임횟수에 포함하지 않는다.
+게임은 5번 진행하고 5번째 게임을 끝내면 다시할지 물어본다. 재시작(1), 종료(0)
+0, 1 이외의 숫자를 입력하면 재입력을 요구해야 한다.
+*/
+public class QuRockPaperScissors {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		int gameCount = 0; 
+		
+		while(true) {
+			//1.난수생성
+			int com = random.nextInt(10000) % 3 + 1;//1~3사이의 난수
+			System.out.println("컴퓨터:"+ com);
+			
+			//2.사용자입력
+			int user = 0;
+			System.out.println("가위바위보를 입력하세요.");
+			System.out.print("가위(1), 바위(2), 보(3)=>");
+			user = scanner.nextInt();
+				
+			//3.승부판단
+			//사용자가 정상적으로 입력한 경우
+			if(!(user<1 || user>3)) {
+				//승부판단 로직1 : if문을 통해 승부를 판단한다.
+				if(com==1&&user==1)	System.out.print("사용자:가위,컴퓨터:가위\n비겼습니다.\n");
+				if(com==1&&user==2) System.out.print("사용자:바위,컴퓨터:가위\n이겼습니다.\n");
+				if(com==1&&user==3) System.out.print("사용자:보,컴퓨터:가위\n졌습니다.\n");
+				if(com==2&&user==1) System.out.print("사용자:가위,컴퓨터:바위\n졌습니다.\n");
+				if(com==2&&user==2) System.out.print("사용자:바위,컴퓨터:바위\n비겼습니다.\n");
+				if(com==2&&user==3) System.out.print("사용자:보,컴퓨터:바위\n이겼습니다.\n");
+				if(com==3&&user==1) System.out.print("사용자:가위,컴퓨터:보\n이겼습니다.\n");
+				if(com==3&&user==2) System.out.print("사용자:바위,컴퓨터:보\n졌습니다.\n");
+				if(com==3&&user==3) System.out.print("사용자:보,컴퓨터:보\n비겼습니다.\n");
+				
+				gameCount++;//게임카운트 증가		
+			}
+			else {
+				System.out.println("가위바위보 할줄 몰라요?? 제대로 내세요 ㅋ");
+			}
+		
+		
+			//4.게임재시작 여부 확인			 
+			if(gameCount>=5) {
+				//0과 1을 입력하지 않아도 게임은 재시작된다. 
+				System.out.print("게임재시작(1), 종료(0):");
+				int restart = scanner.nextInt();
+				if(restart==0) {
+					//게임종료를 위해 while루프 탈출
+					break;
+				}
+				else {
+					System.out.println("게임 재시작^^");
+					gameCount=0;
+				}			
+			}
+		}
+	}
+}
